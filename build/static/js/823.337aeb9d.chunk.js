6a604962f2238bcf30e3a0c69a07f0a263bd03b1
gogi999 <g.petrovic9@gmail.com>
2024-02-13 11:29:49 +0100

    Added simple-blog app

---
+++ build/static/js/823.337aeb9d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_temp=self.webpackChunkreact_temp||[]).push([[823],{823:(t,s,e)=>{e.r(s),e.d(s,{default:()=>i,loader:()=>a});var c=e(560),n=e(12);const o="PostList_list__zl9Ee";var r=e(496);const l=function(t){let{posts:s}=t;return(0,r.jsx)("ul",{className:o,children:s.map((t=>(0,r.jsx)("li",{children:(0,r.jsx)(n.cH,{to:t.id.toString(),children:t.title})},t.id)))})};const i=function(){const t=(0,c.GY)();return(0,r.jsx)(l,{posts:t})};function a(){return fetch("https://jsonplaceholder.typicode.com/posts")}}}]);
+//# sourceMappingURL=823.337aeb9d.chunk.js.map
